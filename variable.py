d4901d74d504f547489977d8db76138ace565e98
hema23z <50hfvhidl@gmail.com>
2026-01-12 12:55:09 +0200

    variable.py

---
+++ variable.py
@@ -0,0 +1,17 @@
+savings =100
+monthly_savings =10
+num_months = 4
+half = 0.5
+new_savings = monthly_savings * num_months
+intro = "Hello! How are you?"
+is_good = True
+total_savings = savings + new_savings
+doubleintro = intro + intro
+
+print (savings)
+print (new_savings)
+print (total_savings)
+print (type(half))
+print (type(total_savings))
+print (doubleintro)
+
