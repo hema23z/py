8367e5f861c47a0325a4e49b08b08e98808fe068
hema23z <50hfvhidl@gmail.com>
2026-01-12 14:02:00 +0200

    Lists.py

---
+++ Lists.py
@@ -0,0 +1,21 @@
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+areas=[hall,kit,liv,bed,bath]
+
+print (areas)
+
+areas = ["hallway", hall,"kitchen", kit, "living room", liv,"bedroom", bed, "bathroom", bath]
+print (areas)
+
+
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom",bed],
+         ["bathroom",bath],      
+        ]
+print (house)
