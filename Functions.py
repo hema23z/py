3c9eaef7d2b5ddf052f2194f44b7dfe1ddc59159
hema23z <50hfvhidl@gmail.com>
2026-01-19 12:52:56 +0200

    Functions.py

---
+++ Functions.py
@@ -0,0 +1,18 @@
+var1 = [1, 2, 3, 4]
+var2 = True
+out2 = int (var2)
+print (type(var1))
+print (type(var2))
+print (type(out2))
+print (len(var1))
+
+
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+full = first + second
+full_sorted = sorted(full,reverse=True)
+full_v = sorted(full)
+print (full)
+print (full_sorted)
+print (full_v)
