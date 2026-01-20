5c02116829670e4776996790c813f557b86aea88
hema23z <50hfvhidl@gmail.com>
2026-01-20 14:24:53 +0200

    Functions.py

---
+++ Methods.py
@@ -0,0 +1,14 @@
+place = "poolhouse"
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+place_up = place.upper()
+print (place)
+print(place_up)
+print (place.count("o"))
+print(areas.index(20.0))
+print(areas.count(9.50))
+
+areas.append(24.5)
+areas.append(15.45)
+print (areas)
+areas.reverse()
+print (areas)
